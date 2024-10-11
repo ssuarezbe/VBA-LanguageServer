be87b1696acc76b34ea9ad14ea4409ca980d0661
Sebastian Suarez <ssuarezbe@gmail.com>
2024-10-11 14:50:09 -0500

    - add rpc interface 	modified:   package-lock.json 	modified:   package.json 	new file:   server/src/rpc_server.ts 	modified:   server/src/server.ts 	modified:   server/tsconfig.tsbuildinfo

---
+++ server/src/rpc_server.ts
@@ -0,0 +1,67 @@
+import { MessageReader, MessageWriter } from "vscode-jsonrpc";
+import * as ws from "ws";
+import * as express from "express";
+import * as rpc from "vscode-ws-jsonrpc";
+import { LanguageServer } from "./server";
+import * as http from "http";
+import * as url from "url";
+import * as net from "net";
+
+function launch(socket: rpc.IWebSocket) {
+  /*
+  https://github.com/railsware/upterm/blob/1d04bcb6d5aec8f62e4025ddee4a77db8cc12c52/src/language-server/ShellLanguageServer.ts#L27
+  */
+  const reader = new rpc.WebSocketMessageReader(socket);
+  const writer = new rpc.WebSocketMessageWriter(socket);
+  const languageServer = new LanguageServer(reader, writer);
+}
+process.on("uncaughtException", function (err: any) {
+  console.error("Uncaught Exception: ", err.toString());
+  if (err.stack) {
+    console.error(err.stack);
+  }
+});
+//
+// create the express application
+const app = express();
+// server the static content, i.e. index.html
+app.use(express.static(__dirname));
+// start the server
+const server = app.listen(3000, "localhost");
+// create the web socket
+const wss = new ws.Server({
+  noServer: true,
+  perMessageDeflate: false,
+});
+//
+server.on(
+  "upgrade",
+  (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
+    const pathname = request.url ? url.parse(request.url).pathname : undefined;
+    if (pathname === "//sampleServer") {
+      wss.handleUpgrade(request, socket, head, (webSocket) => {
+        const socket: rpc.IWebSocket = {
+          send: (content) =>
+            webSocket.send(content, (error) => {
+              if (error) {
+                throw error;
+              }
+            }),
+          onMessage: (cb) => webSocket.on("message", cb),
+          onError: (cb) => webSocket.on("error", cb),
+          onClose: (cb) => webSocket.on("close", cb),
+          dispose: () => {
+            console.log("disposing");
+            webSocket.close();
+          },
+        };
+        // launch the server when the web socket is opened
+        if (webSocket.readyState === webSocket.OPEN) {
+          launch(socket);
+        } else {
+          webSocket.on("open", () => launch(socket));
+        }
+      });
+    }
+  },
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,104 +3,112 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 import {
-	createConnection,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	TextDocumentSyncKind,
-	InitializeResult,
-	ServerCapabilities,
-} from 'vscode-languageserver/node';
+  createConnection,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  TextDocumentSyncKind,
+  InitializeResult,
+  ServerCapabilities,
+  MessageReader,
+  MessageWriter,
+} from "vscode-languageserver/node";
 
-import { Workspace } from './project/workspace';
-import { activateSemanticTokenProvider } from './capabilities/semanticTokens';
-import { activateWorkspaceFolderCapability } from './capabilities/workspaceFolder';
+import { Workspace } from "./project/workspace";
+import { activateSemanticTokenProvider } from "./capabilities/semanticTokens";
+import { activateWorkspaceFolderCapability } from "./capabilities/workspaceFolder";
 
-class LanguageServer {
-	workspace?: Workspace;
-	configuration?: LanguageServerConfiguration;
-	readonly connection;
+export class LanguageServer {
+  workspace?: Workspace;
+  configuration?: LanguageServerConfiguration;
+  readonly connection;
 
-	constructor() {
-		this.connection = createConnection(ProposedFeatures.all);
-		this.connection.onInitialize((params: InitializeParams) => {
-			// Set up the workspace.
-			this.configuration = new LanguageServerConfiguration(params);
-			const workspace = new Workspace({
-				connection: this.connection,
-				capabilities: this.configuration
-			});
-			this.workspace = workspace;
-			
-			// Set up the connection result.
-			// Update this to make use of the LSCapabilities data class.
-			const result = new ConnectionInitializeResult(this.configuration.capabilities);
-			activateWorkspaceFolderCapability(params.capabilities, result);
-			activateSemanticTokenProvider(result);
-			return result;
-		});
-		this.connection.onInitialized(() => {
-			// Register for client configuration notification changes.
-			this.connection.client.register(DidChangeConfigurationNotification.type, undefined);
-			
-		});
+  constructor(reader: MessageReader, writer: MessageWriter) {
+    // Create a connection for the server. The connection uses Node's IPC as a transport.
+    // Also include all preview / proposed LSP features.
+    //this.connection = createConnection(ProposedFeatures.all);
+    this.connection = createConnection(reader, writer);
+    this.connection.onInitialize((params: InitializeParams) => {
+      // Set up the workspace.
+      this.configuration = new LanguageServerConfiguration(params);
+      const workspace = new Workspace({
+        connection: this.connection,
+        capabilities: this.configuration,
+      });
+      this.workspace = workspace;
 
-		this.connection.listen();
-	}
+      // Set up the connection result.
+      // Update this to make use of the LSCapabilities data class.
+      const result = new ConnectionInitializeResult(
+        this.configuration.capabilities,
+      );
+      activateWorkspaceFolderCapability(params.capabilities, result);
+      activateSemanticTokenProvider(result);
+      return result;
+    });
+    this.connection.onInitialized(() => {
+      // Register for client configuration notification changes.
+      this.connection.client.register(
+        DidChangeConfigurationNotification.type,
+        undefined,
+      );
+    });
+
+    this.connection.listen();
+  }
 }
 
 export class LanguageServerConfiguration {
-	params: InitializeParams;
-	capabilities: ServerCapabilities<any> = {
-		// Implemented
-		documentSymbolProvider: true,
-		foldingRangeProvider: true,
-		textDocumentSync: TextDocumentSyncKind.Incremental,
-		
-		// Implement soon.
-		codeActionProvider: false,
-		completionProvider: undefined,
-		hoverProvider: false,
+  params: InitializeParams;
+  capabilities: ServerCapabilities<any> = {
+    // Implemented
+    documentSymbolProvider: true,
+    foldingRangeProvider: true,
+    textDocumentSync: TextDocumentSyncKind.Incremental,
 
-		// Not implemented.		
-		signatureHelpProvider: undefined,
-		declarationProvider: false,
-		definitionProvider: false,
-		typeDefinitionProvider: false,
-		implementationProvider: false,
-		referencesProvider: false,
-		documentHighlightProvider: false,
-		codeLensProvider: undefined,
-		documentLinkProvider: undefined,
-		colorProvider: false,
-		workspaceSymbolProvider: false,
-		documentFormattingProvider: false,
-		documentRangeFormattingProvider: false,
-		documentOnTypeFormattingProvider: undefined,
-		renameProvider: false,
-		selectionRangeProvider: false,
-		executeCommandProvider: undefined,
-		callHierarchyProvider: false,
-		linkedEditingRangeProvider: false,
-		workspace: undefined,
-		monikerProvider: false,
-		experimental: undefined,
-	};
+    // Implement soon.
+    codeActionProvider: false,
+    completionProvider: undefined,
+    hoverProvider: false,
 
-	constructor(params: InitializeParams) {
-		this.params = params;
-	}
+    // Not implemented.
+    signatureHelpProvider: undefined,
+    declarationProvider: false,
+    definitionProvider: false,
+    typeDefinitionProvider: false,
+    implementationProvider: false,
+    referencesProvider: false,
+    documentHighlightProvider: false,
+    codeLensProvider: undefined,
+    documentLinkProvider: undefined,
+    colorProvider: false,
+    workspaceSymbolProvider: false,
+    documentFormattingProvider: false,
+    documentRangeFormattingProvider: false,
+    documentOnTypeFormattingProvider: undefined,
+    renameProvider: false,
+    selectionRangeProvider: false,
+    executeCommandProvider: undefined,
+    callHierarchyProvider: false,
+    linkedEditingRangeProvider: false,
+    workspace: undefined,
+    monikerProvider: false,
+    experimental: undefined,
+  };
+
+  constructor(params: InitializeParams) {
+    this.params = params;
+  }
 }
 
 class ConnectionInitializeResult implements InitializeResult {
-	[custom: string]: any;
-	capabilities: ServerCapabilities<any>;
-	serverInfo?: { name: string; version?: string; };
+  [custom: string]: any;
+  capabilities: ServerCapabilities<any>;
+  serverInfo?: { name: string; version?: string };
 
-	constructor(capabilities: ServerCapabilities) {
-		this.capabilities = capabilities;
-	}
+  constructor(capabilities: ServerCapabilities) {
+    this.capabilities = capabilities;
+  }
 }
 
-
-const languageServer = new LanguageServer();
+//const languageServer = new LanguageServer();
